c8bf74e62804e60bc12e66a5efb2e8574bb1db26
Renan <renan.g.rodrigues5@gmail.com>
2025-05-18 19:03:13 -0300

    18/05

--- 04-05-Javita/src/Revendo/operadoresLogicos.java
+++ 04-05-Javita/src/Revendo/operadoresLogicos.java
@@ -10,6 +10,15 @@ public class operadoresLogicos {
         System.out.println(!condicao); // alterna o tipo do valor boleano
         System.out.println(condicao && codicaoTeste); // verificando se os dois são Verdadeiro
         System.out.println(condicao || codicaoTeste); // ele da true pq ele compara o primeiro e o segundo
-        System.out.println(condicao ^ condicao);
+        System.out.println(condicao ^ codicaoTeste); // Quando usado com booleanos, ^ é o XOR lógico: retorna true se apenas um dos dois operandos for true.
+
+        // Operdores Unarios
+
+        // ++
+        // --
+        int teste = 10;
+        // String Resultado = teste  >= 7.0 ""
+
+
     }
 }
